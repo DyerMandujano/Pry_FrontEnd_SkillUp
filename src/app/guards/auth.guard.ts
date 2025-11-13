59a4aeaf423ecbc8153c0a33d50c2641b77207e4
Abraham Cardona <abrahamjca020@gmail.com>
2025-11-13 09:32:42 -0500

    Update: Pagina principal2

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service'; //
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.isLoggedIn()) {
-    // Opcional: Verificar si es estudiante
-    // const user = authService.getCurrentUser(); // Necesitarías un método en AuthService para esto
-    // if (user.rol === 'estudiante') {
-    //   return true;
-    // }
-    return true; // Por ahora, solo comprobamos si está logueado
+    return true;
   } else {
     // Si no hay sesión, redirige al login
     router.navigate(['/login']);
